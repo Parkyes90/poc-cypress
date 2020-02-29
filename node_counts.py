d46d9a17b5541dd9a166b49009685eef0d432af0
parkyes90 <parkyes90@gmail.com>
2020-03-01 01:07:23 +0900

    feat: Add node_counts
    get node counts

---
+++ node_counts.py
@@ -0,0 +1,45 @@
+import requests
+import time
+import matplotlib.pyplot as plt
+from multiprocessing import Pool
+
+pages = 100
+
+if __name__ == "__main__":
+    result = []
+    before = time.time()
+
+    def get_data(n):
+        p_result = []
+        url = (
+            f"https://bitnodes.earn.com/api/v1/snapshots/"
+            f"?limit=100&page={n}"
+        )
+        res = requests.get(url)
+        data = res.json()
+        print(f"page {n} loaded")
+        for item in data["results"]:
+            ts = item["timestamp"]
+            p_result.append((ts, item["total_nodes"]))
+
+        return p_result
+
+    p = Pool(processes=32)
+    process_result = p.map(get_data, (range(1, pages)))
+    for pr in process_result:
+        for r in pr:
+            result.append(r)
+    result.sort(key=lambda x: x[0])
+    nodes = list(map(lambda x: x[1], result))
+    dates = list(
+        map(
+            lambda x: time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(x[0])),
+            result,
+        )
+    )
+    print(time.time() - before)
+    plt.figure(figsize=(8, 6))
+    plt.plot(nodes, color="red", linewidth=0.7)
+    plt.title(f"Bitcoin Nodes {dates[0]} ~ {dates[-1]}")
+    plt.grid(color="green", alpha=0.2)
+    plt.show()
