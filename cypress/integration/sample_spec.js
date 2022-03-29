3e7416afff5ae8b836a7b5dd3fed9e3020c13fbd
parkyes90 <parkyes90@gmail.com>
2022-03-29 14:18:23 +0900

    Add cypress test example

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,6 +1,13 @@
-describe("First Test", () => {
-    it("Visits the Kitchen Sink", () => {
-        cy.visit("https://example.cypress.io"
-    )
+describe("테스트 1", () => {
+    it("테스트 세부사항 1", () => {
+        cy.visit("/")
+    })
+    it("테스트 세부사항 2", () => {
+        cy.visit("/")
+        cy.contains("Learn React")
+    })
+    it("테스트 세부사항 3", () => {
+         cy.visit("/")
+        cy.contains("Learn React123")
     })
 })
